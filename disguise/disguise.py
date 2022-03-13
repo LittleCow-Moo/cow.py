232de1f894fd283fbefcc608a4c1ebbe460bd9e9
AWeirdScratcher <90096971+AWeirdScratcher@users.noreply.github.com>
2022-03-13 20:12:33 +0800

    Disguise is really owo

--- disguise/disguise.py
+++ disguise/disguise.py
@@ -2,13 +2,27 @@ import requests, json
 
 main = json.loads(open("process.json", "r").read())
 
-print("This is not a valid avatar URL, changed to default avatar.") if not main['avatar_url'].startswith("https://") or not main['avatar_url'].startswith("http://") else None
+
+try:
+    condition = main['avatar_url'].startswith('https://') or main['avatar_url'].startswith('http://')
+except AttributeError:
+    condition = True
+
+i = "This is not a valid avatar URL, changed to default avatar." if condition == True else ""
+print(i)
+
+if condition:
+    main['avatar_url'] = ''
 
 data = {
   "username": main['username'],
   "avatar_url": main['avatar_url'],
   "content": main['content']
 }
-requests.post(main['url'], json=data)
 
-print("Message Sent!")
+try:
+    r = requests.post(main['url'], json=data)
+    r.raise_for_status()
+    print("Message Sent!")
+except Exception as error:
+    print("Error: " + str(error))
